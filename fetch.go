ee95e6ab368aea6ca97008673ef3e631e9b0f856
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2021-11-26 15:21:06 +0100

    fix fetch code being broken
    Also cleans it up a lot.

--- fetch.go
+++ fetch.go
@@ -3,12 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"math"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
+	"io"
 )
 
 type timepoint struct {
@@ -16,64 +13,10 @@ type timepoint struct {
 	Precipation float64
 }
 
-func fetchTwoHours(latitude float64, longitude float64) ([]timepoint, error) {
-	url := fmt.Sprintf("https://gpsgadget.buienradar.nl/data/raintext?lat=%f&lon=%f", latitude, longitude)
-	resp, err := http.Get(url)
-	if err != nil {
-		return []timepoint{}, err
-	}
-	defer resp.Body.Close()
+func parseResponse(r io.ReadCloser) ([]timepoint, error){
+	defer r.Close()
 
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return []timepoint{}, err
-	}
-	lines := strings.Split(string(bytes), "\n")
-
-	parseLine := func(line string) (timepoint, error) {
-		fields := strings.Split(line, "|")
-
-		val, err := strconv.Atoi(fields[0])
-		if err != nil {
-			return timepoint{}, err
-		}
-
-		time, err := time.Parse("15:04", fields[1])
-		if err != nil {
-			return timepoint{}, err
-		}
-
-		return timepoint{
-			Time:        time,
-			Precipation: math.Pow(10, (float64(val)-109)/32),
-		}, nil
-	}
-
-	var res []timepoint
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
-
-		point, err := parseLine(line)
-		if err != nil {
-			return []timepoint{}, err
-		}
-		res = append(res, point)
-	}
-	return res, nil
-}
-
-func fetchFullDay(latitude float64, longitude float64) ([]timepoint, error) {
-	url := fmt.Sprintf("https://graphdata.buienradar.nl/2.0/forecast/geo/rain24hour/?lat=%f&lon=%f", latitude, longitude)
-	resp, err := http.Get(url)
-	if err != nil {
-		return []timepoint{}, err
-	}
-	defer resp.Body.Close()
-
-	decoder := json.NewDecoder(resp.Body)
+	decoder := json.NewDecoder(r)
 
 	var item struct {
 		Forecasts []struct {
@@ -98,4 +41,24 @@ func fetchFullDay(latitude float64, longitude float64) ([]timepoint, error) {
 		})
 	}
 	return res, nil
+}
+
+func fetchTwoHours(latitude float64, longitude float64) ([]timepoint, error) {
+	url := fmt.Sprintf("https://graphdata.buienradar.nl/2.0/forecast/geo/rain/?lat=%f&lon=%f", latitude, longitude)
+	resp, err := http.Get(url)
+	if err != nil {
+		return []timepoint{}, err
+	}
+
+	return parseResponse(resp.Body)
+}
+
+func fetchFullDay(latitude float64, longitude float64) ([]timepoint, error) {
+	url := fmt.Sprintf("https://graphdata.buienradar.nl/2.0/forecast/geo/rain24hour/?lat=%f&lon=%f", latitude, longitude)
+	resp, err := http.Get(url)
+	if err != nil {
+		return []timepoint{}, err
+	}
+
+	return parseResponse(resp.Body)
 }
